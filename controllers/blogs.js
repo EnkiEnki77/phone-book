e22cbc1fedad56e711f35094b3e9ce36689f66c1
Enki Layman <layman212@gmail.com>
2023-06-01 09:03:25 -0400

    added getAll and post routes

---
+++ controllers/blogs.js
@@ -0,0 +1,38 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const { info } = require('../utils/logger');
+
+blogsRouter.get('/', (req, res) => {
+    Blog.find({})
+    .then(result => res.json(result))
+});
+
+blogsRouter.post('/', (req, res, next) => {
+    const body = req.body
+
+    if(body.title == null){
+        res.status(400).send({error: 'Title is missing'})
+    }else if(body.author == null){
+        res.status(400).send({error: 'Author is missing'})
+    }else if(body.url == null){
+        res.status(400).send({error: 'URL is missing'})
+    }else if(body.likes == null){
+        res.status(400).send({error: 'Likes is missing'})
+    }
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    })
+
+    blog.save()
+        .then(result => {
+            info('Added new blog')
+            res.json(result)
+        })
+        .catch(err => next(err))
+})
+
+module.exports = blogsRouter;
--- controllers/persons.js
+++
@@ -1,9 +0,0 @@
-const personsRouter = require('express').Router();
-const Person = require('../models/person');
-
-personsRouter.get('/', (req, res) => {
-    Person.find({})
-    .then(result => res.json(result))
-});
-
-module.exports = personsRouter;
