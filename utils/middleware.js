971dd48c1e843288bc2fb28ae1109b1cd10f9b89
Enki Layman <layman212@gmail.com>
2023-05-31 23:24:39 -0400

    setup middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,13 +1,27 @@
-const requestLogger = (req, res, next) => {
+const { info, error } = require("./logger")
 
+const requestLogger = (req, res, next) => {
+    info('Method:', req.method)
+    info('Path:', req.path)
+    info('Body:', req.body)
+    info('...')
+    next()
 }
 
 const unknownEndpoint = (req, res, next) => {
-    
+    res.status(404).send({error: 'Unknown endpoint'})
 }
 
-const errorHandler = (error, req, res, next) => {
-    
+const errorHandler = (err, req, res, next) => {
+    error(err.message)
+
+    if(err.name == 'CastError'){
+        return res.status(400).send({error: 'malformed ID'})
+    }else if(err.name == 'ValidationError'){
+        return res.status(400).send({error: err.message})
+    }
+
+    next(err)
 }
 
 module.exports = {requestLogger, unknownEndpoint, errorHandler}
