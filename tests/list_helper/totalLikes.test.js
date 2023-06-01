ac477bc1d0b6a7c5514d9cc4680da1dba14b1f56
Enki Layman <layman212@gmail.com>
2023-06-01 10:15:33 -0400

    completed part 4 exercise 4

---
+++ tests/list_helper/totalLikes.test.js
@@ -0,0 +1,69 @@
+const {totalLikes} = require('../../utils/list_helper')
+
+describe('Total likes', () => {
+    const listWithNoBlog= []
+
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+          }
+    ]
+
+    const listWithMultipleBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+          },
+          {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+          },
+          {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+          },
+          {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+          }
+    ]
+
+    test('of empty list is 0', () => { 
+        const result = totalLikes(listWithNoBlog)
+
+        expect(result).toBe(0)
+     })
+
+    test('when list has only one blog, equals the likes of that', () => { 
+        const result = totalLikes(listWithOneBlog)
+
+        expect(result).toBe(5)
+     })
+
+     test('of a bigger list is calculated correctly', () => { 
+        const result = totalLikes(listWithMultipleBlogs)
+
+        expect(result).toBe(20)
+     })
+})
